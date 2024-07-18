06f4a19b9b8fe69d04bab0433fe2f97962d542d6
Ali9731 <alisadeghi.work@gmail.com>
2024-07-19 02:08:16 +0330

    localize added

---
+++ lang/fa/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'card_not_found' => 'شماره کارت وارد شده در سیستم موجود نمیباشد : :CARD',
+    'delivery_time_error' => 'سفارش شما در حال ارسال میباشد',
+    'report_exists_error' => 'تاخیر سفارش شما در حال پیگیزی میباشد',
+    'no_delay_reports_error' => ' رکوردی یافت نشد',
+    'record_not_found' => 'نتیجه ای یافت نشد',
+    'new_estimate' => 'سفارش شما تا :ESTIMATE دقیقه دیگر به دستتان خواهد رسید',
+    'delay_reported' => 'تاخیر سفارش شما در حال بررسی است و نتیجه اعلام خواهد شد',
+    'agent_have_active_report' => 'شما یک تاخیر بررسی نشده دارید',
+    'agent_id.required' => 'فیلد ای دی پشتیبان اجباری میباشد',
+    'order_id.required' => 'فیلد ای دی سفارش اجباری میباشد',
+
+];
