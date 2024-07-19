e914ba1bc30b858d046ed9ddced8e563a059ff23
Ali9731 <alisadeghi.work@gmail.com>
2024-07-19 04:58:51 +0330

    Repositories and Services Registered

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Repositories\Agent\AgentRepository;
+use App\Repositories\Agent\AgentRepositoryInterface;
+use App\Repositories\DelayReport\DelayReportRepository;
+use App\Repositories\DelayReport\DelayReportRepositoryInterface;
+use App\Repositories\Order\OrderRepository;
+use App\Repositories\Order\OrderRepositoryInterface;
+use App\Repositories\Vendor\VendorRepository;
+use App\Repositories\Vendor\VendorRepositoryInterface;
+use App\Services\DelayReport\DelayReportService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+        $this->app->bind(DelayReportRepositoryInterface::class, DelayReportRepository::class);
+        $this->app->bind(AgentRepositoryInterface::class, AgentRepository::class);
+        $this->app->bind(VendorRepositoryInterface::class, VendorRepository::class);
+        $this->app->bind(DelayReportService::class, function ($app) {
+            return new DelayReportService($app->make(DelayReportRepositoryInterface::class));
+        });
     }
 
     /**
