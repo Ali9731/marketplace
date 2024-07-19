993ae8ae66a823bc8c94684eada4e613cf596778
Ali9731 <alisadeghi.work@gmail.com>
2024-07-19 04:14:52 +0330

    routes added

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AgentController;
+use App\Http\Controllers\Api\DelayReportController;
+use App\Http\Controllers\Api\VendorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => '/v1'], function () {
+
+    Route::post('/delay-report', [DelayReportController::class, 'report']);
+    Route::post('/agent-assign', [AgentController::class, 'assign']);
+    Route::get('/vendor-delays', [VendorController::class, 'vendorDelays']);
+
+});
